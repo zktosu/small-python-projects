77974a73ca20f3929df5f36a26a95c9d2ef6e6d4
zktosu <zktosu@gmail.com>
2023-09-09 20:56:22 +0300

    break timer implemented

--- break_timer.py
+++ break_timer.py
@@ -1,5 +1,14 @@
 # get time
 # set interval
 # alarm at interval
-seconds = input("Time Interval - Seconds ")
-
+import time
+seconds = int(input("Time Interval - Seconds "))
+time-to-sleep = 1
+s = seconds
+while True:
+	s = seconds
+	while s:
+		time.sleep(1)
+		s -= 1
+	print("Time for break!!!")
+	time.sleep(time-to-sleep)
