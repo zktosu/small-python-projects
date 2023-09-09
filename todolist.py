2522101b1577264a3b5e6aa99cb73061062a3686
zktosu <zktosu@gmail.com>
2023-09-09 22:10:30 +0300

    todos application finished

--- todolist.py
+++ todolist.py
@@ -3,8 +3,17 @@ conn = sqlite3.connect(":memory:")
 cur = conn.cursor()
 cur.execute("create table todos(id integer primary key, todo char(300), status boolean default False)")
 # get task from user.
-cur.execute("insert into todos(todo) values('say hi')")
+while True:
+	operation = input("Select Operation {a:add,d:done} ")
+	if operation == 'a':
+		input_val = input("please type the task ")
+		cur.execute("insert into todos(todo) values(?)",(input_val,))
+	if operation == 'd':
+		input_id = input("please give id of task done ")
+		cur.execute("delete from todos where id=?",(input_id,))
 # select operation addition, status change
-cur.execute("select * from todos where status=False")
-for item in cur:
-	print(item[0],item[1])
+	cur.execute("select * from todos where status=False")
+	for item in cur:
+		print(item[0],item[1])
+	if operation=='q':
+		break
