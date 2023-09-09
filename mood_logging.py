1e8a19dd5c530d2ce2ee5a75b07cdcf1982d1915
zktosu <zktosu@gmail.com>
2023-09-09 21:21:31 +0300

    added time.ctime for insertion

--- mood_logging.py
+++ mood_logging.py
@@ -3,12 +3,15 @@
 # get mood time it as now
 # record to :memory: database.
 import sqlite3
+import time 
 conn = sqlite3.connect(":memory:")
 cur = conn.cursor()
-cur.execute("create table moods(time int primary key not null, mood char(100) not null)")
+cur.execute("create table moods(time char(100) primary key not null, mood char(100) not null)")
 while True:
 	mood = input("Please type your mood (happy,sad,angry etc)")
-	mood_time = 12 #datetime.now() 
+	if mood == "q":
+		break
+	mood_time = time.ctime() 
 	cur.execute("insert into moods(time, mood) values(?,?)",(mood_time,mood))
 	cur.execute("select * from moods")
 	for item in cur.fetchall():
