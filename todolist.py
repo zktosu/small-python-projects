8f53a1dd64ad4b42858167fe1d1a027f005d3aaf
zktosu <zktosu@gmail.com>
2023-09-09 21:45:06 +0300

    added plan

--- todolist.py
+++ todolist.py
@@ -1,9 +1,10 @@
 import sqlite3
 conn = sqlite3.connect(":memory:")
 cur = conn.cursor()
-cur.execute("create table todos(id integer primary key, todo char(300))")
-
+cur.execute("create table todos(id integer primary key, todo char(300), status boolean default False)")
+# get task from user.
 cur.execute("insert into todos(todo) values('say hi')")
-cur.execute("select * from todos")
+# select operation addition, status change
+cur.execute("select * from todos where status=False")
 for item in cur:
 	print(item[0],item[1])
