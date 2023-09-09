965bdc03d91a12eb8e89c05b101bfe8791b75604
zktosu <zktosu@gmail.com>
2023-09-09 21:39:57 +0300

    todolist: skeleton built

--- todolist.py
+++ todolist.py
@@ -1,1 +1,9 @@
-pass
+import sqlite3
+conn = sqlite3.connect(":memory:")
+cur = conn.cursor()
+cur.execute("create table todos(id integer primary key, todo char(300))")
+
+cur.execute("insert into todos(todo) values('say hi')")
+cur.execute("select * from todos")
+for item in cur:
+	print(item[0],item[1])
