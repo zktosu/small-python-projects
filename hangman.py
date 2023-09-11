2bf49e164ecbad3de246036aa28dec02d6c1f8a7
zktosu <zktosu@gmail.com>
2023-09-11 18:50:39 +0300

    hangman: found a way to print given letters -- should use multi letter input

--- hangman.py
+++ hangman.py
@@ -1,13 +1,14 @@
 import random
-words = ["sample","life","boring","stuff","member"]
+# words = ["sample","life","boring","stuff","member"]
+words = ["sample"]
 word = random.sample(words,1)[0]
 res = "-"*len(word)
 while True:
 	letter = input("Enter a letter:")
 	res = ""
 	for i in word:
-		if i == letter:
-			res += letter
+		if i in letter:
+			res += i 
 		else:
 			res += "-"
 	print(res)
