0fddd01a738d0b4f33f4f1cedc0d6dc6efc5d5e4
zktosu <zktosu@gmail.com>
2023-09-11 20:29:27 +0300

    contactbook: clean up

--- contactbook.py
+++ contactbook.py
@@ -5,18 +5,22 @@ create table contacts(id integer primary key autoincrement,name char(100),addres
 """)
 cur = conn.cursor()
 # cur.execute("insert into contacts(name,address,phone) values(?,?,?)",("name","address","23040324"))
-# cur.execute("select * from contacts")
+
 
 while True:
-	option = input("Please select operation: (a)dd, (r)emove")
+	option = input("Please select operation: (a)dd, (l)ist, (r)emove: ")
 	if option == "a":
-		name = input("please enter name")
-		address = input("please enter an address")
-		phone = input("please enter phone number")
+		name = input("please enter name: ")
+		address = input("please enter an address: ")
+		phone = input("please enter phone number: ")
 		cur.execute("insert into contacts(name,address,phone) values(?,?,?)",(name, address, phone))
 	if option == "r":
-		name = input("Please enter a name to remove")
+		name = input("Please enter a name to remove: ")
 		cur.execute("delete from contacts where name = ?",(name,))
-	for item in cur.fetchall():
-		print(item[0],item[1],item[2],item[3])
-
+	if option == "l":
+		cur.execute("select * from contacts")
+		for item in cur.fetchall():
+			print(item[0],item[1],item[2],item[3])
+	if option == "q":
+		print("You have choosen to exit")
+		break
