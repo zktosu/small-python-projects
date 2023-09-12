29722d9bf4ffbd4ec28654417495b69dde11acfb
zktosu <zktosu@gmail.com>
2023-09-12 20:36:12 +0300

    hangman2: first attempt

--- hangman2.py
+++ hangman2.py
@@ -1,1 +1,26 @@
-pass
+# show secret word with dashes (-)
+# get a letter from user (a letter)
+# check if letter is gussed already
+# add letter to guessed letters list
+# update dashed word. replace - for correct letters.
+# loop
+word = "award"
+dword = "-"*len(word)
+guessed_letters = ""
+while True:
+	print(dword)
+	letter = input("Give me a letter: ")
+	if len(letter) != 1 or letter in guessed_letters:
+		print("Error, letter used or you have given too much")
+		continue
+	guessed_letters += letter
+	for i in range(len(word)):
+		if letter == word[i]:
+			# this is the way to replace dash with letter
+			dword = dword[:i] + letter + dword[i+1:]
+	if len(guessed_letters) == 5:
+		print("You have used all guesses")
+		break
+	if dword == word:
+		print("Well Done!")
+		
