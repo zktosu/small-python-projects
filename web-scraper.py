302c8d3a1b6b1f13eb4acb182be66791a81dbeb5
zktosu <zktosu@gmail.com>
2023-09-13 20:04:56 +0300

    webscraper comment added

--- web-scraper.py
+++ web-scraper.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 address = input("give the address to get the content: ")
 r = requests.get("https://"+address)
 soup = BeautifulSoup(r.text,'html.parser')
+# get all the links on a page.
 links = soup.find_all('a')
 for link in links:
 	print(link.get("href"))
