502e5977fa506550e03331a8276b62005456d7c4
zktosu <zktosu@gmail.com>
2023-09-13 19:57:39 +0300

    webscraper: added beautiful soup to parse html requests to get the page

--- web-scraper.py
+++ web-scraper.py
@@ -1,5 +1,9 @@
 import requests
-
-address = input("give the address to get the content")
+from bs4 import BeautifulSoup
+address = input("give the address to get the content: ")
 r = requests.get("https://"+address)
-print(r.text)
+soup = BeautifulSoup(r.text,'html.parser')
+links = soup.find_all('a')
+for link in links:
+	print(link.get("href"))
+
