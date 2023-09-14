44b4061d355d68bbcffce85639ef3208d1f491bb
zktosu <zktosu@gmail.com>
2023-09-14 17:41:51 +0300

    gui-hello: QPushButton and simple Slot with pyside6

--- gui-hello.py
+++ gui-hello.py
@@ -1,6 +1,11 @@
-from PySide6.QtWidgets import QLabel,QApplication
+from PySide6.QtWidgets import QPushButton,QLabel,QApplication
+from PySide6.QtCore import Slot
 
+@Slot()
+def hey():
+    print("Say Hey!")
 app = QApplication()
-label = QLabel("<center><h1>Hello World!!</h1></center>")
-label.show()
+button = QPushButton("<h1>Say Hi</h1>")
+button.clicked.connect(hey)
+button.show()
 app.exec()
